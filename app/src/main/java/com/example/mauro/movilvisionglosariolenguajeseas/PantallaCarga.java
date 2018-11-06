f7226eb7f825b17b705811ed903e220f6e9a1cc2
Paola <pbrengifo25@gmail.com>
2018-11-05 21:33:14 -0600

    Paola-Rengifo
    Pantalla de Carga- aumenta rueda de carga

--- app/src/main/java/com/example/mauro/movilvisionglosariolenguajeseas/PantallaCarga.java
+++ app/src/main/java/com/example/mauro/movilvisionglosariolenguajeseas/PantallaCarga.java
@@ -1,12 +1,16 @@
 package com.example.mauro.movilvisionglosariolenguajeseas;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class PantallaCarga extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +20,42 @@ public class PantallaCarga extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(PantallaCarga.this,Main2Activity.class);
+                new AsyncTask_load().execute();
                 startActivity(intent);
-
                 finish();
             }
-        },4000);
+        },3000);
+    }
+
+    public class AsyncTask_load extends AsyncTask<Void,Integer,Void> {
+        int progress;
+        @Override
+        protected void onPreExecute() {
+            progress=0;
+            progressBar.setVisibility(View.VISIBLE);
+
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            while(progress<100)
+            {
+                progress++;
+                publishProgress(progress);
+                SystemClock.sleep(30);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 }
